f25693b066978789a18e6beffb1d7c690e1878a4
Patricio Flores <141748067+G-h0st1n@users.noreply.github.com>
2026-02-19 08:50:50 -0600

    made query clearer

--- sql_challenges/challenge-02/solution.sql
+++ sql_challenges/challenge-02/solution.sql
@@ -32,8 +32,8 @@ LEFT JOIN Employees
     ON buildings.building_name = employees.building;
 
 --INTERVIEW QUESTIONS
-SELECT pages.page_id, user_id, page_likes.page_id
+SELECT pages.page_id
 FROM pages
 LEFT JOIN page_likes ON pages.page_id = page_likes.page_id
-WHERE user_id IS NULL
+WHERE page_likes.page_id IS NULL
 ORDER BY pages.page_id ASC;
