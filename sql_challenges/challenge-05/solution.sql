20926e99ec1a460d6a4d2206bb74ceb26f2532ce
Patricio Flores <141748067+G-h0st1n@users.noreply.github.com>
2026-03-12 08:12:29 -0600

    session-2026-03-12

---
+++ sql_challenges/challenge-05/solution.sql
@@ -0,0 +1,24 @@
+--FreeSQL
+
+--Question 1
+select colour from my_brick_collection
+union
+select colour from your_brick_collection
+order by colour;
+
+
+select shape from my_brick_collection
+union all
+select shape from your_brick_collection
+order  by shape;
+
+--Question 2
+select shape from my_brick_collection
+minus
+select shape from your_brick_collection;
+
+
+select colour from my_brick_collection
+intersect
+select colour from your_brick_collection
+order  by colour;
